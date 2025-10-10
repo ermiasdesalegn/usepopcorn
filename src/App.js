c06d2a996423cd7c90ff79870994ccef76792628
ermiasdesalegnn <ermiasdesalegnn@gmail.com>
2025-10-10 09:36:57 +0300

    more refactoring taken place

--- src/App.js
+++ src/App.js
@@ -59,24 +59,40 @@ export default function App() {
   );
 }
 function NavBar() {
-  const [query, setQuery] = useState("");
   return (
     <nav className="nav-bar">
-      <div className="logo">
-        <span role="img">🍿</span>
-        <h1>usePopcorn</h1>
-      </div>
-      <input
-        className="search"
-        type="text"
-        placeholder="Search movies..."
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      <p className="num-results">
-        Found <strong>x</strong> results
-      </p>
+      <Logo />
+      <Search />
+      <Result />
     </nav>
+  );
+}
+
+function Logo() {
+  return (
+    <div className="logo">
+      <span role="img">🍿</span>
+      <h1>usePopcorn</h1>
+    </div>
+  );
+}
+function Search() {
+  const [query, setQuery] = useState("");
+  return (
+    <input
+      className="search"
+      type="text"
+      placeholder="Search movies..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+    />
+  );
+}
+function Result() {
+  return (
+    <p className="num-results">
+      Found <strong>x</strong> results
+    </p>
   );
 }
 

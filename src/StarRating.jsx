c21bab579f34800cc6458f8567e446a00f473f17
ermiasdesalegnn <ermiasdesalegnn@gmail.com>
2025-10-17 23:20:36 +0300

    the hovering started and now works fine

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -34,13 +34,13 @@ export default function StarRating({ maxRating = 5 }) {
           <Star
             key={i}
             onRating={() => handleRating(i + 1)}
-            full={rating >= i + 1}
-            onHoverIn={setRating(i + 1)}
-            onHoverOut={setRating(0)}
+            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+            onHoverIn={() => setRating(i + 1)}
+            onHoverOut={() => setRating(0)}
           />
         ))}
       </div>
-      <p style={textStyle}>{rating || ""}</p>
+      <p style={textStyle}>{tempRating || ""}</p>
     </div>
   );
 }

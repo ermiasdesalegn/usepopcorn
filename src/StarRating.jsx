d4446b291aa28eea471e0fd5e27775113db8153c
ermiasdesalegnn <ermiasdesalegnn@gmail.com>
2025-10-18 17:59:30 +0300

    the message is now working

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -14,8 +14,10 @@ export default function StarRating({
   maxRating = 5,
   color = `#fcc419`,
   size = 48,
+  messages = [],
+  defaultRating = 0,
 }) {
-  const [rating, setRating] = useState(0);
+  const [rating, setRating] = useState(defaultRating);
   const [tempRating, setTempRating] = useState(0);
 
   function handleRating(rating) {
@@ -44,7 +46,11 @@ export default function StarRating({
           />
         ))}
       </div>
-      <p style={textStyle}>{tempRating || rating || ""}</p>
+      <p style={textStyle}>
+        {messages.length === maxRating
+          ? messages[tempRating ? tempRating - 1 : rating - 1]
+          : tempRating || rating || ""}
+      </p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["terrible", "bad", "Okay", "Good", "Amazing"]}
+    />
+
+    <StarRating maxRating={5} color="red" className="test" defaultRating={3} />
   </React.StrictMode>
 );

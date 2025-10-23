daa7bcddf8adf23995a0da8f5bf5bcf03c9bdf2a
ermiasdesalegnn <ermiasdesalegnn@gmail.com>
2025-10-23 22:45:27 +0300

    the featching is on progress

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
 
-  fetch(`http://www.omdbapi.com/?apikey=${key}&s=inception`).then(res=>res.json().then(data=>console.log(data)))
+  featch(`http://www.omdbapi.com/?apikey=${key}&s=inception`).then(res=>res.json().then(data=>console.log(data)))
   .catch(err=>console.error(err));
 
   return (

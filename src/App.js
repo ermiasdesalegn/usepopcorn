7abe829c860158db305da261c33c325bad30f3f2
ermiasdesalegn <ermiasdesalegnn@gmail.com>
2026-02-26 23:00:08 +0300

    the thing about adding a selected movie to my watched list is progressing

--- src/App.js
+++ src/App.js
@@ -101,6 +101,17 @@ export default function App() {
     },
     [query],
   );
+
+  useEffect(function () {
+    async function fetchMovie() {
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=${key}&i=${selectedId}`,
+      );
+      console.log(res.data);
+    }
+    fetchMovie(selectedId);
+  }, []);
+
   return (
     <>
       <NavBar>
@@ -258,11 +269,42 @@ function WatchedMovie({ movie }) {
 }
 
 function MovieDetails({ selectedId, onCloseMovie }) {
+  const [movie, setMovie] = useState({});
+
+  const {
+    Title: title,
+    Year: year,
+    Poster: poster,
+    Runtime: runtime,
+    imdbRating,
+    Plot: plot,
+    Released: released,
+    Actors: actors,
+    Director: director,
+    Genre: genre,
+  } = movie;
+
+  // console.log(title, year);
+
+  useEffect(function () {
+    async function fetchMovie() {
+      const res = await fetch(
+        `http://www.omdbapi.com/?apikey=${key}&i=${selectedId}`,
+      );
+      const data = await res.json();
+      setMovie(data);
+    }
+    fetchMovie(selectedId);
+  }, []);
+
   return (
     <div className="details">
-      <button className="btn-back" onClick={onCloseMovie}>
-        &larr;
-      </button>
+      <header>
+        <button className="btn-back" onClick={onCloseMovie}>
+          &larr;
+        </button>
+        <img src={poster} alt={`poster of ${movie} movie`} />
+      </header>
       {selectedId}
     </div>
   );

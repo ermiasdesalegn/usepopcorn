a07f0cfef1833f63b7cbaca63edfd6868191921b
ermiasdesalegn <ermiasdesalegnn@gmail.com>
2026-03-07 11:56:36 +0300

    the custom hook for local storage is enhanced

--- src/useLocalStorageState.js
+++ src/useLocalStorageState.js
@@ -1,6 +1,8 @@
-export function useLocalStorageState(watched) {
+import { useState } from "react";
+export function useLocalStorageState() {
   const [watched, setWatched] = useState(function () {
     const storedWatched = localStorage.getItem("watched");
     return JSON.parse(storedWatched);
   });
+  return [watched, setWatched];
 }

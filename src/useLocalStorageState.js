fee06f9942b481a6c6bac56451a0a88ca455fb59
ermiasdesalegn <ermiasdesalegnn@gmail.com>
2026-03-07 12:17:15 +0300

    the custom hook for local storage is enhanced and done well

--- src/useLocalStorageState.js
+++ src/useLocalStorageState.js
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 export function useLocalStorageState(initialState, key) {
   const [value, setValue] = useState(function () {
     const storedValue = localStorage.getItem(key);
-    return JSON.parse(storedValue);
+    console.log(storedValue);
+    return storedValue ? JSON.parse(storedValue) : initialState;
   });
   // return [value, setValue];
   useEffect(
